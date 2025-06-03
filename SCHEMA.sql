f01f8d5318ad06e5dcd0f02bc4904479d2963ade
Abdelrahman <abdelrahmamohamed309@gmail.com>
2025-06-03 08:34:33 +0300

    Updated schema with latest changes

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -40,4 +40,16 @@ is_optional char(1) CHECK (is_optional IN ('Y' , 'N')));
 
 SELECT * FROM PRODUCT_INGREDIENT_details;
 
----SELECT * FROM USER_constraints
+CREATE TABLE  feedback_customers (
+feedback_id int PRIMARY KEY , 
+cust_id int REFERENCES customers (cust_id), 
+order_id int REFERENCES orders (order_id), 
+rating decimal(10,2) , 
+ comm varchar2(100) , 
+feedback_date DATE );
+
+ALTER TABLE FEEDBACK_CUSTOMERS 
+RENAME COLUMN comm
+TO  feedback_comment;
+ALTER TABLE FEEDBACK_CUSTOMERS 
+MODIFY (feedback_comment VARCHAR2(200));
